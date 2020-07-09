774177623c4a0d7093da8aa8eeb1b8475ab16371
ThaiSonP <tysonpan@pursuit.org>
2020-07-09 03:36:54 -0400

    defining a function using setCash and seeing its range within app

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -3,10 +3,14 @@ import React, {useState} from 'react';
 const App = props =>{
   const [cash, setCash]=useState(0);
 
+  const plusOne = ()=>{
+    setCash(cash+1)
+  }
+
 return(
   <div>
     <p>current balance: {cash}</p>
-    <button onClick ={()=>setCash(cash+1)}>button</button>
+    <button onClick ={()=>plusOne()}>button</button>
   </div>
   );
 }
